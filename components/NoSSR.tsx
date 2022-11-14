cff7268c6390add0bae198ce5d33128b4bc3b3dc
Rafael Valerio <rafvalerio26@gmail.com>
2022-11-14 23:04:46 +1100

    Fixing type

--- components/NoSSR.tsx
+++ components/NoSSR.tsx
@@ -1,7 +1,11 @@
 import dynamic from 'next/dynamic'
 import React from 'react'
 
-const NoSsr = ({ children }) => <>{children}</>
+type Props = {
+  children: React.ReactNode
+}
+
+const NoSsr = ({ children }: Props) => <>{children}</>
 
 export default dynamic(() => Promise.resolve(NoSsr), {
   ssr: false,
